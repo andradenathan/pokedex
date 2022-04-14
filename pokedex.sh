4b580ad547d28fbacce3e8fa2359e24cf2795409
Nathan Andrade <42661561+andradenathan@users.noreply.github.com>
2022-04-13 22:19:39 -0300

    Create pokedex.sh

---
+++ pokedex.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "You need to specify an argument to search your pokemon"
+    exit 1
+fi
+
+url="https://pokeapi.co/api/v2/pokemon/$1"
+
+data=$(curl -s $url)
+
+echo "$data" | jq -r '(["Name", "Height", "Weight", "Types"] | 
+    (., map(length* "-"))), (
+        [.name, .height, .weight, (.types[].type.name)]) | @tsv' | column -t
